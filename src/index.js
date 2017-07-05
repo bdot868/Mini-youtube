37dc87fffb498a52658f4d57f56103f39b0f72fe
bdot868 <brandoncarter100@gmail.com>
2017-07-05 00:45:28 -0700

    replaced url with new url containing api key

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-import VideoList from './components/video_list'
-import VideoDetail from './components/video_detail'
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyCo9LMMVVYvuP3DkUyt_JVQIzfMAF35GIc';
 
 
